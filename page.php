3e79118014116cff46c666ac19e5f22864ed06ef
miami-14 <travailmia@gmail.com>
2025-01-06 14:41:56 +0100

    Create page.php

---
+++ page.php
@@ -0,0 +1,34 @@
+<?php
+
+// Charger la fonction pour parser le YAML
+require_once("../yaml/yaml.php");
+
+// Lire le fichier YAML
+$data = yaml_parse_file('page.yaml');
+
+
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page Portfolio </title>
+    <link rel="stylesheet" href="page.css">
+    
+    <h1> Bienvenu sur le projet PORTFOLIO  </h1>
+   
+</head>
+<body>
+
+
+<button> <li><a href="../Accueil/accueil.php" data-text="Acceuil">Acceuil</a></li> </button>
+<button> <li><a href="../Compétences/comptétences.php" data-text="Compétences">Compétence</a></li> </button>
+<button> <li><a href="../Réalisations/réalisation.php" data-text="Réalisation">Réalisation</a></li> </button>
+<button> <li><a href="../Formation/formation.php" data-text="Formation">Formation</a></li> </button>
+<button> <li><a href="../Contact/contact.php" data-text="Contact">Contact</a></li> </button>
+
+        </ul>
+</body>
+</html>
