8f9b9315ccaafe62bde4cf6d3427225d2ad431d3
miami-14 <travailmia@gmail.com>
2025-01-08 21:26:06 +0100

    modification

--- Page/page.php
+++ Page/page.php
@@ -3,7 +3,7 @@
 // Charger la fonction pour parser le YAML
 require_once("../yaml/yaml.php");
 // Lire le fichier YAML bbbbbbbb
-$data = yaml_parse_file('page.yaml');
+$data = yaml_parse_file('Page/page.yaml');
 
 ?>
 
@@ -21,14 +21,13 @@ $data = yaml_parse_file('page.yaml');
 <h1> Bienvenu sur le projet PORTFOLIO  </h1>
 
 
-<button> <li><a href="Accueil/accueil.php" data-text="Acceuil">Acceuil</a></li> </button>
-<button> <li><a href="Compétences/competences.php" data-text="Competences">Compétence</a></li> </button>
-<button> <li><a href="Réalisations/réalisation.php" data-text="Réalisation">Réalisation</a></li> </button>
-<button> <li><a href="Formation/formation.php" data-text="Formation">Formation</a></li> </button>
-<button> <li><a href="Contact/contact.php" data-text="Contact">Contact</a></li> </button>
+<button> <li><a href=../Accueil/accueil.php" data-text="Acceuil">Acceuil</a></li> </button>
+<button> <li><a href="../Compétences/competences.php" data-text="Competences">Compétence</a></li> </button>
+<button> <li><a href="../Réalisations/réalisation.php" data-text="Réalisation">Réalisation</a></li> </button>
+<button> <li><a href="../Formation/formation.php" data-text="Formation">Formation</a></li> </button>
+<button> <li><a href="../Contact/contact.php" data-text="Contact">Contact</a></li> </button>
 
     
 </body>
 </html>
-
 
