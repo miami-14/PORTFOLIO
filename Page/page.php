c3ccb6e74ca3f4f7131bdba97d3a3459e78d16c0
miami-14 <travailmia@gmail.com>
2025-01-08 21:29:20 +0100

    teste

--- Page/page.php
+++ Page/page.php
@@ -21,7 +21,7 @@ $data = yaml_parse_file('Page/page.yaml');
 <h1> Bienvenu sur le projet PORTFOLIO  </h1>
 
 
-<button> <li><a href=../Accueil/accueil.php" data-text="Acceuil">Acceuil</a></li> </button>
+<button> <li><a href="../Accueil/accueil.php" data-text="Acceuil">Acceuil</a></li> </button>
 <button> <li><a href="../Compétences/competences.php" data-text="Competences">Compétence</a></li> </button>
 <button> <li><a href="../Réalisations/réalisation.php" data-text="Réalisation">Réalisation</a></li> </button>
 <button> <li><a href="../Formation/formation.php" data-text="Formation">Formation</a></li> </button>
